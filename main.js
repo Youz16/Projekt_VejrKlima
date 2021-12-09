c2bc7689b635a862a41390f26c9c418ce58111f2
Youzi <Yousefche16@gmail.com>
2021-12-09 16:33:27 +0100

    Trial and error

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ app.use(morgan("combined"));
 app.post("/api/energyData", async (req, res) => {
   try {
     // Lav query
-    const query = `SELECT entity, year, primary_energy_consum FROM energy WHERE code NOT NULL ORDER BY entity;`;
+    const query = `SELECT entity, year, primary_energy_consum FROM energy WHERE code IS NOT NULL ORDER BY entity;`;
     queryData = await client.query(query);
     // Giv svar tilbage til JavaScript
     res.json({
